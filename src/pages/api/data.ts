3533f3569b56fae97307a8e14fda65d1b9bd955b
ANURAG SINGH <apk.anurag.singh@gmail.com>
2023-07-24 02:10:49 +0530

    Added landing page banner

---
+++ src/pages/api/data.ts
@@ -0,0 +1,100 @@
+export const config = {
+  countries: [
+    {
+      name: "India",
+      flag: "/assets/in.png",
+    },
+    {
+      name: "Australia",
+      flag: "/assets/au.png",
+    },
+    {
+      name: "Brasil",
+      flag: "/assets/br.png",
+    },
+    {
+      name: "Canada",
+      flag: "/assets/ca.png",
+    },
+    {
+      name: "Chile",
+      flag: "/assets/cl.png",
+    },
+    {
+      name: "Czech Republic",
+      flag: "/assets/cz.png",
+    },
+    {
+      name: "Indonesia",
+      flag: "/assets/id.png",
+    },
+    {
+      name: "Ireland",
+      flag: "/assets/ie.png",
+    },
+    {
+      name: "Italy",
+      flag: "/assets/it.png",
+    },
+    {
+      name: "Lebanon",
+      flag: "/assets/lb.png",
+    },
+    {
+      name: "Malaysia",
+      flag: "/assets/my.png",
+    },
+    {
+      name: "New Zealand",
+      flag: "/assets/nz.png",
+    },
+    {
+      name: "Philippines",
+      flag: "/assets/ph.png",
+    },
+    {
+      name: "Poland",
+      flag: "/assets/pl.png",
+    },
+    {
+      name: "Portugal",
+      flag: "/assets/pt.png",
+    },
+    {
+      name: "Qatar",
+      flag: "/assets/qa.png",
+    },
+    {
+      name: "Singapore",
+      flag: "/assets/sg.png",
+    },
+    {
+      name: "Slovakia",
+      flag: "/assets/sk.png",
+    },
+    {
+      name: "South Africa",
+      flag: "/assets/za.png",
+    },
+    {
+      name: "Sri Lanka",
+      flag: "/assets/lk.png",
+    },
+    {
+      name: "Turkey",
+      flag: "/assets/tr.png",
+    },
+    {
+      name: "UAE",
+      flag: "/assets/ae.png",
+    },
+    {
+      name: "United Kingdom",
+      flag: "/assets/gb.png",
+    },
+    {
+      name: "United States",
+      flag: "/assets/us.png",
+    },
+  ],
+};
